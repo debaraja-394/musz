3a31e3db791d6218e326674250cf86e849514553
debaraja-394 <debabrat827@gmail.com>
2025-03-10 16:53:59 +0530

    Removed expo completely

--- frontend/app/screens/PlayerScreen.jsx
+++
@@ -1,9 +0,0 @@
-import { View, StyleSheet, Text, TouchableOpacity } from "react-native";
-import Slider from '@react-native-community/slider';
-export default function PlayerScreen() {
-    return (
-        <View>
-
-        </View>
-    )
-}
